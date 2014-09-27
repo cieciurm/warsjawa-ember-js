9c3b7ca0544a56cd26ca756c42ce2fb7da203d7e
Mateusz Cieciura <mateusz5000@gmail.com>
2014-09-27 16:52:58 +0200

    summ work

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+  this.resource("subreddit", {path: "/subreddit/:name"});
   // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+	return subreddits;
   }
+});
+
+subreddits = [
+	{key: 'cityporn', name: 'City Porn'},
+	{key: 'askreddit', name: 'Ask Reddit'},
+	{key: 'aww', name: 'Aww'}
+]
+
+App.IndexController = Ember.ArrayController.extend({
+	actions: {
+	  clickMe: function() {
+		  alert('Hello ember.js');
+	  }
+	}
 });
